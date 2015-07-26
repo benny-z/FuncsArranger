7101848bb1de7858fec3b9c7e2ef44fad97bb6cb
benny-z <benny-z@users.noreply.github.com>
2015-07-26 16:16:26 +0300

    getting funcs size from object files

--- GetFuncLengths.py
+++ GetFuncLengths.py
@@ -1,12 +1,26 @@
+from pprint import pprint
 from os import chdir, walk
-from os.path import dirname, join
+from os.path import dirname, join, basename
 import subprocess
 import fnmatch
+import re
+
+FUNC_SIZE_NAME_PATTERN = re.compile('([\w\d]+)\s\w\s[\d|a-fA-F]+\s([\d|a-fA-F]+)')
+
+def parse_file(file_content):
+	func_to_size_map = {}
+	for func_name, func_size in re.findall(FUNC_SIZE_NAME_PATTERN, file_content):
+		func_to_size_map.update({func_name : int(func_size,16) })
+	return func_to_size_map
 
 def get_all_funcs(obj_files_lst):
+	filename_to_funcs_size = {}
 	for o_f_name in obj_files_lst:
-		result = subprocess.check_output(['nm' ,'-S', '-f', 'posix', o_f_name])
-		print(result)
+		o_file_content = subprocess.check_output(['nm' ,'-S', '-f', 'posix', o_f_name])
+		func_name_to_size_map = parse_file(o_file_content)
+		filename_to_funcs_size.update({basename(o_f_name) : func_name_to_size_map})
+	return filename_to_funcs_size
+		
 
 def get_obj_files_lst(base_dir):
 	matches = []
@@ -17,7 +31,8 @@ def get_obj_files_lst(base_dir):
 
 if '__main__' == __name__:
 	cur_dir = dirname(__file__)
+	cur_dir = cur_dir if cur_dir != '' else '.'
 	chdir(cur_dir)
 
 	obj_files_lst = get_obj_files_lst(cur_dir)
-	get_all_funcs(obj_files_lst)
+	pprint(get_all_funcs(obj_files_lst))
